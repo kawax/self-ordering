32f22fbd036c399c4aec99c4c3706914bf244e50
kawax <kawaxbiz@gmail.com>
2021-01-31 17:02:42 +0900

    help

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -4,11 +4,12 @@
             <h1 class="text-3xl">{{ config('app.name', 'Laravel') }}</h1>
         </x-slot>
 
-        <div class="my-6">
-            スマホでQRコードを読み込んでください。
+        <div class="text-center">
+            <div class="mb-6">
+                スマホでQRコードを読み込んでください。
+            </div>
+
+            <x-ordering::qr :url="route('order')"></x-ordering::qr>
         </div>
-
-        <x-ordering::qr :url="route('order')"></x-ordering::qr>
-
     </x-ordering::card>
 </x-ordering-guest-layout>
