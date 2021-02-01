421a8ab978b0cd594e0c673baea253ef7585dc80
kawax <kawaxbiz@gmail.com>
2021-02-01 16:34:16 +0900

    header

--- resources/views/order/header.blade.php
+++ resources/views/order/header.blade.php
@@ -1,4 +1,4 @@
-<header class="p-10 text-white text-center bg-black bg-gradient-to-r from-yellow-500  to-red-500">
+<header class="p-10 text-white text-center bg-black bg-gradient-to-r from-primary-50 to-primary-900">
     <h1 class="text-3xl">{{ config('app.name', 'Laravel') }}</h1>
     <div>
         テーブル : {{ session('table') }}
