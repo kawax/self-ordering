d9b4d5b95478383f7ded00b07b4f41dd6e0ae3bd
kawax <kawaxbiz@gmail.com>
2021-02-24 14:47:22 +0900

    noindex

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="robots" content="noindex">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
