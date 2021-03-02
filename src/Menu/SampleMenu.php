e9db913955a4910f6cc42af3ae403f8d45b0a55b
kawax <kawaxbiz@gmail.com>
2021-03-02 17:22:22 +0900

    sample

--- src/Menu/SampleMenu.php
+++ src/Menu/SampleMenu.php
@@ -12,31 +12,31 @@ class SampleMenu implements MenuData
      */
     public function __invoke(): Collection
     {
-        $id = 1;
+        $id = 0;
 
         return collect([
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'テスト1',
                 'price'    => 100,
                 'category' => 'カテゴリーA',
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'テスト2',
                 'text'     => 'あいうえお',
                 'price'    => 100,
                 'category' => 'カテゴリーA',
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'テスト3',
                 'text'     => 'かきくけこさしすせそ',
                 'price'    => 200,
                 'category' => 'カテゴリーA',
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => '牛丼',
                 'text'     => '',
                 'price'    => 200,
@@ -44,14 +44,14 @@ class SampleMenu implements MenuData
                 'image'    => $this->image('food_gyudon.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'そば',
                 'price'    => 200,
                 'category' => 'カテゴリーB',
                 'image'    => $this->image('soba_kake.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'モーニングセット',
                 'text'     => '★モーニング限定',
                 'price'    => 300,
@@ -59,37 +59,37 @@ class SampleMenu implements MenuData
                 'image'    => $this->image('cafe_morning_coffee_set.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'ビーフステーキ',
                 'price'    => 400,
                 'category' => 'カテゴリーD',
                 'image'    => $this->image('food_beefsteak.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'ラザニア（ランチ限定）',
                 'price'    => 500,
                 'category' => 'カテゴリーE',
                 'image'    => $this->image('food_lasagna_razania.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => 'カツカレー',
                 'price'    => 600,
                 'category' => 'カテゴリーF',
                 'image'    => $this->image('food_katsu_curry.png'),
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => '店員へのメッセージ',
                 'text'     => '追加メモに用件を書いて店員呼び出しの代わりにご利用ください（0円で後払いを選択）',
                 'price'    => 0,
                 'category' => '店員呼出',
             ],
             [
-                'id'       => $id++,
+                'id'       => ++$id,
                 'name'     => '注文のキャンセル',
-                'text'     => '注文直後のキャンセルは近くの店員に声をかけるか、こちからメモを書いてお伝えください。',
+                'text'     => '注文直後のキャンセルは近くの店員に声をかけるか、こちらからメモを書いてお伝えください。',
                 'price'    => 0,
                 'category' => '店員呼出',
             ],
